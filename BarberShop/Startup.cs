1bb661713e3785ff5967532c1ae18d0d9eb96ad6
carlosv123 <vasquezcarlos1996@gmail.com>
2017-11-02 11:01:49 -0700

    Project files

---
+++ BarberShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BarberShop.Startup))]
+namespace BarberShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
